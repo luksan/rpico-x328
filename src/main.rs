fee06d8ae6dcc04bd5a988ba59e5ed94852e1eff
Lukas Sandström <lukas.sandstrom@chalmers.se>
2022-08-24 11:56:11 +0200

    Show received x328 read commands on the display

--- src/main.rs
+++ src/main.rs
@@ -49,12 +49,13 @@ mod app {
     use super::*;
     use crate::hal::gpio;
     use crate::pac::CorePeripherals;
+    use embedded_graphics::text::renderer::TextRenderer;
     use embedded_hal::digital::v2::StatefulOutputPin;
     use embedded_hal::prelude::_embedded_hal_blocking_serial_Write;
     use rp2040_monotonic::Rp2040Monotonic;
 
-    type UartBuf = ArrayVec<u8, 20>;
-    pool!(USB_UART_POOL: UartBuf);
+    type DispLine = ArrayString<25>;
+    pool!(DISP_TEXT: DispLine);
 
     const SCAN_TIME_US: u32 = 10000;
 
@@ -78,15 +79,15 @@ mod app {
         screen: pimoroni_pico_explorer::Screen,
         x328_node: x328_proto::node::Node,
         usb_device: UsbDevice<'static, hal::usb::UsbBus>,
-        usb_tx_send: Producer<'static, Box<USB_UART_POOL>, 2>,
-        usb_tx_recv: Consumer<'static, Box<USB_UART_POOL>, 2>,
+        disp_text_tx: Producer<'static, Box<DISP_TEXT>, 2>,
+        disp_text_rx: Consumer<'static, Box<DISP_TEXT>, 2>,
     }
 
     #[init(
         local = [
             usb_bus_uninit: MaybeUninit<usb_device::bus::UsbBusAllocator<hal::usb::UsbBus>> = MaybeUninit::uninit(),
             uart_mem_pool: [u8; 128] = [0; 128],
-            usb_tx_queue: Queue<Box<USB_UART_POOL>, 2> = Queue::new(),
+            usb_tx_queue: Queue<Box<DISP_TEXT>, 2> = Queue::new(),
         ]
     )]
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
@@ -101,7 +102,7 @@ mod app {
             usb_tx_queue,
         } = ctx.local;
 
-        USB_UART_POOL::grow(uart_mem_pool);
+        DISP_TEXT::grow(uart_mem_pool);
 
         let mut pac: Peripherals = ctx.device;
         let cp: CorePeripherals = ctx.core;
@@ -162,14 +163,9 @@ mod app {
             .build();
 
         // The queue for sending USB serial data
-        let (usb_tx_send, usb_tx_recv) = usb_tx_queue.split();
+        let (disp_text_tx, disp_text_rx) = usb_tx_queue.split();
 
         let x328_node = x328_proto::node::Node::new(X328_NODE_ADDR);
-
-        //let mut timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
-        //let mut alarm = timer.alarm_0().unwrap();
-        //          let _ = alarm.schedule(SCAN_TIME_US.microseconds());
-        //        alarm.enable_interrupt();
 
         x328_task::spawn();
         (
@@ -186,8 +182,8 @@ mod app {
                 screen: explorer.screen,
                 x328_node,
                 usb_device,
-                usb_tx_send,
-                usb_tx_recv,
+                disp_text_tx,
+                disp_text_rx,
             },
             init::Monotonics(Systick::new(
                 delay.free(),
@@ -197,13 +193,24 @@ mod app {
     }
 
     // Task with least priority that only runs when nothing else is running.
-    #[idle(local = [gpio_0, screen])]
+    #[idle(local = [gpio_0, screen, disp_text_rx])]
     fn idle(cx: idle::Context) -> ! {
-        // Locals in idle have lifetime 'static
-        // let _x: &'static mut u32 = cx.local.x;
+        let idle::LocalResources {
+            gpio_0,
+            screen,
+            disp_text_rx,
+        } = cx.local;
+        let screen: &mut pimoroni_pico_explorer::Screen = screen;
 
-        //hprintln!("idle").unwrap();
-        let idle::LocalResources { gpio_0, screen } = cx.local;
+        let style = MonoTextStyleBuilder::new()
+            .font(&FONT_10X20)
+            .text_color(Rgb565::GREEN)
+            .background_color(Rgb565::BLACK)
+            .build();
+
+        let mut pos = Point::new(0, style.line_height() as i32 * 5);
+        let start_pos = pos;
+
         loop {
             if Ok(true) == gpio_0.is_set_high() {
                 gpio_0.set_low();
@@ -215,40 +222,41 @@ mod app {
             // Write to buffer
             writeln!(&mut buf, "Hello World").unwrap();
             writeln!(&mut buf, "Temp: {}", gpio_0.is_set_high().unwrap()).unwrap();
-            /*
-            writeln!(
-                &mut buf,
-                "A:{:.1} B:{:.1}\nX:{:.1} Y:{:.1}",
-                explorer.is_pressed(Button::A),
-                explorer.is_pressed(Button::B),
-                explorer.is_pressed(Button::X),
-                explorer.is_pressed(Button::Y)
-            )
-            .unwrap();*/
             // Draw buffer on screen
-            let style = MonoTextStyleBuilder::new()
-                .font(&FONT_10X20)
-                .text_color(Rgb565::GREEN)
-                .background_color(Rgb565::BLACK)
-                .build();
-            Text::with_alignment(&buf, Point::new(20, 30), style, Alignment::Left)
-                .draw(screen)
-                .unwrap();
+            Text::with_alignment(
+                &buf,
+                Point::new(0, style.line_height() as i32),
+                style,
+                Alignment::Left,
+            )
+            .draw(screen)
+            .unwrap();
 
-            cortex_m::asm::nop();
+            while let Some(line) = disp_text_rx.dequeue() {
+                Text::new("  ", pos, style).draw(screen).unwrap();
+                pos.y += style.line_height() as i32;
+                if pos.y >= screen.bounding_box().size.height as i32 {
+                    pos = start_pos;
+                }
+                pos = Text::new("> ", pos, style).draw(screen).unwrap();
+                Text::with_alignment(line.as_ref(), pos, style, Alignment::Left)
+                    .draw(screen)
+                    .unwrap();
+                pos.x = 0;
+            }
         }
     }
 
     #[task(
         priority = 1,
         capacity = 1,
-        local = [x328_node, usb_tx_send, gpio_2],
+        local = [x328_node, disp_text_tx, gpio_2],
         shared = [usb_serial, led]
     )]
     fn x328_task(mut ctx: x328_task::Context) {
         let x328_task::LocalResources {
             x328_node,
-            usb_tx_send,
+            disp_text_tx,
             gpio_2,
         } = ctx.local;
         let x328_node: &mut x328_proto::node::Node = x328_node;
@@ -280,10 +288,18 @@ mod app {
                 NodeState::SendData(send) => serial.lock(|serial| {
                     serial.write(send.get_data());
                 }),
-                NodeState::ReadParameter(read) => match *read.parameter() {
-                    1 => read.send_reply_ok(4u16.into()),
-                    _ => read.send_invalid_parameter(),
-                },
+                NodeState::ReadParameter(read) => {
+                    if let Some(mut line) =
+                        DISP_TEXT::alloc().map(|uninit| uninit.init(DispLine::new()))
+                    {
+                        write!(&mut line, "r {} {}", *read.address(), *read.parameter());
+                        disp_text_tx.enqueue(line);
+                    }
+                    match *read.parameter() {
+                        1 => read.send_reply_ok(4u16.into()),
+                        _ => read.send_invalid_parameter(),
+                    }
+                }
                 NodeState::WriteParameter(write) => match *write.parameter() {
                     _ => write.write_ok(),
                 },
@@ -294,17 +310,13 @@ mod app {
     #[task(
         binds = USBCTRL_IRQ,
         priority=2,
-        local = [usb_device, usb_tx_recv, gpio_1],
+        local = [usb_device, gpio_1],
         shared = [usb_serial],
     )]
     fn usb_irq(mut ctx: usb_irq::Context) {
         use core::sync::atomic::{AtomicBool, Ordering};
 
-        let usb_irq::LocalResources {
-            usb_device,
-            usb_tx_recv,
-            gpio_1,
-        } = ctx.local;
+        let usb_irq::LocalResources { usb_device, gpio_1 } = ctx.local;
 
         let mut serial = ctx.shared.usb_serial;
         // Poll the USB driver with all of our supported USB Classes
@@ -319,79 +331,5 @@ mod app {
             }
             gpio_1.set_low();
         });
-    }
-}
-
-fn main_old() -> ! {
-    // Grab our singleton objects
-    let mut pac = pac::Peripherals::take().unwrap();
-    let cp = pac::CorePeripherals::take().unwrap();
-
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    // Configure the clocks
-    //
-    // The default is to generate a 125 MHz system clock
-    let clocks = hal::clocks::init_clocks_and_plls(
-        XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    // Enable adc
-    let mut adc = Adc::new(pac.ADC, &mut pac.RESETS);
-    let mut temp_sense = adc.enable_temp_sensor();
-
-    let sio = Sio::new(pac.SIO);
-
-    let mut delay = cortex_m::delay::Delay::new(cp.SYST, clocks.system_clock.get_freq().integer());
-    let (mut explorer, pins) = PicoExplorer::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        pac.SPI0,
-        adc,
-        &mut pac.RESETS,
-        &mut delay,
-    );
-
-    let mut led = pins.led.into_push_pull_output();
-
-    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
-
-    loop {
-        let temp_read_adc = explorer.get_adc(&mut temp_sense);
-
-        // Create a fixed buffer to store screen contents
-        let mut buf = ArrayString::<100>::new();
-
-        // Write to buffer
-        writeln!(&mut buf, "Hello World").unwrap();
-        writeln!(&mut buf, "Temp: {}", temp_read_adc).unwrap();
-        writeln!(
-            &mut buf,
-            "A:{:.1} B:{:.1}\nX:{:.1} Y:{:.1}",
-            explorer.is_pressed(Button::A),
-            explorer.is_pressed(Button::B),
-            explorer.is_pressed(Button::X),
-            explorer.is_pressed(Button::Y)
-        )
-        .unwrap();
-        // Draw buffer on screen
-        let style = MonoTextStyleBuilder::new()
-            .font(&FONT_10X20)
-            .text_color(Rgb565::GREEN)
-            .background_color(Rgb565::BLACK)
-            .build();
-        Text::with_alignment(&buf, Point::new(20, 30), style, Alignment::Left)
-            .draw(&mut explorer.screen)
-            .unwrap();
     }
 }
